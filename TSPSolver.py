7133c5ae09b817e877ee80ef2ab6fa845a8f7ca0
imcgill <ihatchmcgill@gmail.com>
2023-04-06 13:38:50 -0600

    reduced cost function

--- TSPSolver.py
+++ TSPSolver.py
@@ -2,15 +2,15 @@
 import copy
 
 from which_pyqt import PYQT_VER
-if PYQT_VER == 'PYQT5':
-	from PyQt5.QtCore import QLineF, QPointF
-elif PYQT_VER == 'PYQT4':
-	from PyQt4.QtCore import QLineF, QPointF
-elif PYQT_VER == 'PYQT6':
-	from PyQt6.QtCore import QLineF, QPointF
-else:
-	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
+if PYQT_VER == 'PYQT5':
+    from PyQt5.QtCore import QLineF, QPointF
+elif PYQT_VER == 'PYQT4':
+    from PyQt4.QtCore import QLineF, QPointF
+elif PYQT_VER == 'PYQT6':
+    from PyQt6.QtCore import QLineF, QPointF
+else:
+    raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
 import time
 import numpy as np
@@ -19,18 +19,14 @@ import heapq
 import itertools
 
 
-
-
-
 class TSPSolver:
-	def __init__( self, gui_view ):
-		self._scenario = None
+    def __init__(self, gui_view):
+        self._scenario = None
 
-	def setupWithScenario( self, scenario ):
-		self._scenario = scenario
+    def setupWithScenario(self, scenario):
+        self._scenario = scenario
 
-
-	''' <summary>
+    ''' <summary>
 		This is the entry point for the default solver
 		which just finds a valid random tour.  Note this could be used to find your
 		initial BSSF.
@@ -41,38 +37,37 @@ class TSPSolver:
 		algorithm</returns>
 	'''
 
-	def defaultRandomTour( self, time_allowance=60.0 ):
-		results = {}
-		cities = self._scenario.getCities()
-		ncities = len(cities)
-		foundTour = False
-		count = 0
-		bssf = None
-		start_time = time.time()
-		while not foundTour and time.time()-start_time < time_allowance:
-			# create a random permutation
-			perm = np.random.permutation( ncities )
-			route = []
-			# Now build the route using the random permutation
-			for i in range( ncities ):
-				route.append( cities[ perm[i] ] )
-			bssf = TSPSolution(route)
-			count += 1
-			if bssf.cost < np.inf:
-				# Found a valid route
-				foundTour = True
-		end_time = time.time()
-		results['cost'] = bssf.cost if foundTour else math.inf
-		results['time'] = end_time - start_time
-		results['count'] = count
-		results['soln'] = bssf
-		results['max'] = None
-		results['total'] = None
-		results['pruned'] = None
-		return results
+    def defaultRandomTour(self, time_allowance=60.0):
+        results = {}
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        foundTour = False
+        count = 0
+        bssf = None
+        start_time = time.time()
+        while not foundTour and time.time() - start_time < time_allowance:
+            # create a random permutation
+            perm = np.random.permutation(ncities)
+            route = []
+            # Now build the route using the random permutation
+            for i in range(ncities):
+                route.append(cities[perm[i]])
+            bssf = TSPSolution(route)
+            count += 1
+            if bssf.cost < np.inf:
+                # Found a valid route
+                foundTour = True
+        end_time = time.time()
+        results['cost'] = bssf.cost if foundTour else math.inf
+        results['time'] = end_time - start_time
+        results['count'] = count
+        results['soln'] = bssf
+        results['max'] = None
+        results['total'] = None
+        results['pruned'] = None
+        return results
 
-
-	''' <summary>
+    ''' <summary>
 		This is the entry point for the greedy solver, which you must implement for
 		the group project (but it is probably a good idea to just do it for the branch-and
 		bound project as a way to get your feet wet).  Note this could be used to find your
@@ -84,41 +79,39 @@ class TSPSolver:
 		algorithm</returns>
 	'''
 
-	def greedy( self,time_allowance=60.0 ):
-		results = {}
-		cities = self._scenario.getCities()
-		visited = []
-		not_visited = copy.copy(cities)
+    def greedy(self, time_allowance=60.0):
+        results = {}
+        cities = self._scenario.getCities()
+        visited = []
+        not_visited = copy.copy(cities)
 
-		city = cities[0]
-		visited.append(city)
-		not_visited.remove(city)
+        city = cities[0]
+        visited.append(city)
+        not_visited.remove(city)
 
-		start_time = time.time()
-		while len(not_visited) > 0 and time.time()-start_time < time_allowance:
-			next_city = min(not_visited, key=lambda x: city.costTo(x))
+        start_time = time.time()
+        while len(not_visited) > 0 and time.time() - start_time < time_allowance:
+            next_city = min(not_visited, key=lambda x: city.costTo(x))
 
-			if city.costTo(next_city) == float('inf'):
-				break
-			visited.append(next_city)
-			not_visited.remove(next_city)
-			city = next_city
-		end_time = time.time()
+            if city.costTo(next_city) == float('inf'):
+                break
+            visited.append(next_city)
+            not_visited.remove(next_city)
+            city = next_city
+        end_time = time.time()
 
-		bssf = TSPSolution(visited)
-		results['cost'] = bssf.cost if not bssf.cost == np.inf else math.inf
-		results['time'] = end_time - start_time
-		results['count'] = 1
-		results['soln'] = bssf
-		results['max'] = None
-		results['total'] = None
-		results['pruned'] = None
+        bssf = TSPSolution(visited)
+        results['cost'] = bssf.cost if not bssf.cost == np.inf else math.inf
+        results['time'] = end_time - start_time
+        results['count'] = 1
+        results['soln'] = bssf
+        results['max'] = None
+        results['total'] = None
+        results['pruned'] = None
 
-		return results
+        return results
 
-
-
-	''' <summary>
+    ''' <summary>
 		This is the entry point for the branch-and-bound algorithm that you will implement
 		</summary>
 		<returns>results dictionary for GUI that contains three ints: cost of best solution,
@@ -127,29 +120,54 @@ class TSPSolver:
 		max queue size, total number of states created, and number of pruned states.</returns>
 	'''
 
-	def branchAndBound( self, time_allowance=60.0 ):
-		cities = self._scenario.getCities()
-		start_time = time.time()
-		results = self.greedy()
-		if results['cost'] == math.inf:
-			# Greedy Algorithm was unable to find a valid solution, use random default solution instead.
-			results = self.defaultRandomTour()
+    def branchAndBound(self, time_allowance=60.0):
+        cities = self._scenario.getCities()
+        start_time = time.time()
+        results = self.greedy()
+        if results['cost'] == math.inf:
+            # Greedy Algorithm was unable to find a valid solution, use random default solution instead.
+            results = self.defaultRandomTour()
+        bssf = results['cost']
 
-		# Ideally place in separate function
-		cost_matrix = self.get_cost_matrix()
-		pass
+        pq = []
+        reduced_matrix, lower_bound = self.get_initial_state()
 
-	def get_cost_matrix(self):
-		cities = self._scenario.getCities()
-		ncities = len(cities)
-		distance_matrix = [[float('inf') for j in range(ncities)] for i in range(ncities)]
+        # Create dictionary and push onto pq
 
-		for i in range(ncities):
-			for j in range(ncities):
-				distance_matrix[i][j] = cities[i].costTo(cities[j])
-		return distance_matrix
+        pass
 
-	''' <summary>
+    def get_initial_state(self):
+        cities = self._scenario.getCities()
+        ncities = len(cities)
+        distance_matrix = [[float('inf') for j in range(ncities)] for i in range(ncities)]
+
+        for i in range(ncities):
+            for j in range(ncities):
+                distance_matrix[i][j] = cities[i].costTo(cities[j])
+
+        distance_matrix, lower_bound = self.reduce_matrix(distance_matrix)
+
+        return distance_matrix, lower_bound
+
+    def reduce_matrix(self, matrix):
+        # Subtract the minimum value from each row and each column
+        # Find the minimum value in each row
+        row_min = [min(row) for row in matrix]
+        # Subtract the minimum value from each element in the row
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                matrix[i][j] -= row_min[i]
+        # Find the minimum value in each column
+        col_min = [min(col) for col in zip(*matrix)]
+        # Subtract the minimum value from each element in the column
+        for j in range(len(matrix)):
+            for i in range(len(matrix[j])):
+                matrix[i][j] -= col_min[j]
+
+        lower_bound = sum(row_min) + sum(col_min)
+        return matrix, lower_bound
+
+    ''' <summary>
 		This is the entry point for the algorithm you'll write for your group project.
 		</summary>
 		<returns>results dictionary for GUI that contains three ints: cost of best solution,
@@ -158,7 +176,5 @@ class TSPSolver:
 		algorithm</returns>
 	'''
 
-	def fancy( self,time_allowance=60.0 ):
-		pass
-
-
+    def fancy(self, time_allowance=60.0):
+        pass
